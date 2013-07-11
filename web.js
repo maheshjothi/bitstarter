1b13a2fd29b5380d51703d9ad21e0268ed826eba
Mahesh J <maheshjothi@gmail.com>
2013-07-11 17:52:21 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,13 +2,10 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+fs.readFile('./index.html', 'utf8', function (err, html) {
+    if(err){
+                console.log(err);
+            }else{
+                console.log(html);
+            }
 });
